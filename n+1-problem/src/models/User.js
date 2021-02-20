e9caf7701850f626d4dbfbe019541fc66ad6b858
Juan Imsand <juan.imsand@hotmail.com>
2021-02-20 14:37:25 -0300

    n+1-problem

---
+++ n+1-problem/src/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3
+    },
+    favoriteGenre: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 4
+    }
+})
+
+module.exports = mongoose.model('User', schema)
